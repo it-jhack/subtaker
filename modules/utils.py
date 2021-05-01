0fe18ceccfe9a9fc45b2c3ab09154aec3199cb7a
Thiago A <it.eng.amaraldepaula@gmail.com>
2021-05-01 09:28:12 -0300

    refactor modulate bruteforce, add screen messages

--- modules/utils.py
+++ modules/utils.py
@@ -8,7 +8,7 @@ def get_time_yymmdd_hhmm():
     return datetime.now().strftime('%y%m%d-%H%M')
 
 def screen_msg(message):
-    print(f'{get_time_hhmm()} {message}')
+    print(f'{get_time_hhmm()} > {message}')
 
 def exec_and_readlines(cmd, domains):
     domains_str = bytes('\n'.join(domains), 'ascii')
@@ -17,9 +17,18 @@ def exec_and_readlines(cmd, domains):
 
     return [j.decode('utf-8').strip() for j in stdout.splitlines() if j != b'\n']
 
+# Merge 2 or more lists, remove duplicates
 def merge_lists(main_list, *args):
+    
     for arg in args:
         main_list.extend(arg)
         main_list = list(unique_everseen(main_list))
     
     return main_list
+
+# Merge 2 lists, remove duplicates, count how many added new elements
+def merge_and_count_new_unique(main_list, new_list):
+    old_main_len = len(main_list)
+    main_list = merge_lists(main_list, new_list)
+
+    return main_list, len(main_list) - old_main_len
