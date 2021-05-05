353032520be929014dc3f51d92cc78eef85165f7
Thiago A <it.eng.amaraldepaula@gmail.com>
2021-05-05 20:36:34 -0300

    add jq parsing func subp with filter, shell NOT needed

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from more_itertools import unique_everseen
+import subprocess
 
 
 def get_time_hhmm():
@@ -23,16 +24,6 @@ def get_time_yymmdd_hhmmss():
 def screen_msg(message):
     '''Helpful to print events while running the program'''
     print(f'{get_time_hhmm()} > {message}')
-
-
-def exec_and_readlines(cmd, domains):
-    '''Executes a command, having a list of domains or subdomains as argument'''
-
-    domains_str = bytes('\n'.join(domains), 'ascii')
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, stdin=subprocess.PIPE)
-    stdout, stderr = proc.communicate(input=domains_str)
-
-    return [j.decode('utf-8').strip() for j in stdout.splitlines() if j != b'\n']
 
 
 def merge_lists(list1, list2, *args):
@@ -72,6 +63,11 @@ def count_lines(file_path):
 
 def write_full_output(file_path, string_output):
     with open(file_path, 'w') as f:
+        f.write(str(string_output))
+
+
+def write_list(file_path, string_output):
+    with open(file_path, 'w') as f:
         f.write(string_output)
 
 
@@ -85,3 +81,37 @@ def append_list(file_path, list_output):
     with open(file_path, 'a') as f:
         for item in list_output:
             f.write(item + '\n')
+
+
+def get_ndjson_jq(file_path):
+    p = subprocess.run([f'grep NOERROR {NDJ_OUTP_1}\
+    | jq -crM \'.data | .answers | .[] | .name\'\
+    | sort\
+    | uniq'],
+        shell=True, capture_output=True, text=True)
+
+
+def get_jq(jq_filter, file_path):
+    # Reusing the same 'p' var to save memory on large outputs
+    p = subprocess.run(['grep', 'NOERROR', file_path], capture_output=True)
+
+    p = subprocess.run([
+        'jq', '-crM', jq_filter 
+            # Returns 'bytes' type
+            # -c: compact instead of pretty-printed output;
+            # -r: output raw strings, not JSON texts;
+            # -M: monochrome (don't colorize JSON);
+    ],
+    capture_output=True, input=p.stdout)
+
+    p = subprocess.run(['sort'], capture_output=True, input=p.stdout)
+    p = subprocess.run(['uniq'], capture_output=True, input=p.stdout)
+
+    p = str(p.stdout).strip('b\'')
+    
+    results_l = []
+
+    results_l.extend(p.split('\\n'))
+    results_l.remove('') # remove any empty element left on list
+
+    return results_l
