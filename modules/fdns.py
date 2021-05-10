eb9cf8cf1e5e57ffd0fd09c8d473b0dbc947e8d3
Thiago A <it.eng.amaraldepaula@gmail.com>
2021-05-10 15:40:57 -0300

    fix (fdns): subprocess refeed causing mem kill

--- modules/fdns.py
+++ modules/fdns.py
@@ -1,16 +1,16 @@
-
 import subprocess
 import modules.utils
+from more_itertools import unique_everseen
 
 
 # Converts subdomain text in order to be parsed by grep and jq commands
 # It is important to keep the same domains order in both 'grep_list' and 'jq_regex_list'
 
-def _subdomain_convert(domains_list):
+def _grep_jq_convert(domains_list):
     '''Transforms a list of items into two lists: intended for 'grep' and 'jq' subprocess, respectively.
     Be careful to parse both lists at the same order, such as by using the commands:
 
-    grep_list, jq_regex_list = _subdomain_convert(domains_list)
+    grep_list, jq_regex_list = _grep_jq_convert(domains_list)
     
     for grep_item, jq_item in zip(grep_list, jq_regex_list):'''
 
@@ -24,10 +24,10 @@ def _subdomain_convert(domains_list):
         grep_str = '.'       
         jq_regex_str = '\\\\.'
         i = 1
-        for item in sliced_scope:
+        for item in sliced_domain:
             grep_str += item
             jq_regex_str += item
-            if i < len(sliced_scope):
+            if i < len(sliced_domain):
                 grep_str += '.'
                 jq_regex_str += '\\\\.'
                 i += 1
@@ -41,29 +41,25 @@ def grep_subds_fdns(domains_list, fdns_file):
     '''Parses a 'Rapid7 Open Data FDNS' database file and returns a list of subdomains.
     
     See more at https://opendata.rapid7.com'''
+    
+    #! Dev Warning 0: 'shell=True' was needed to avoid mem kill, 
+        # as opposed to using multiple 'p = subprocess.run' and 'input=p.stdout' method
+    
+    #! Dev Warning 1: this step may heavily load the CPU. TODO try multithreading in the future'
 
     fdns_outp_list = []
-    grep_list, jq_regex_list = _subdomain_convert(domains_list)
+    grep_list, jq_regex_list = _grep_jq_convert(domains_list)
     
     for grep_item, jq_item in zip(grep_list, jq_regex_list):
 
-        p = subprocess.run(['zcat', fdns_file], capture_output=True)
-        p = subprocess.run(['grep', '-F', grep_item], capture_output=True, input=p.stdout)
+        jq_filter = f'\'if (.name | test("{jq_item}")) then .name elif (.value | test("{jq_item}")) then .value else empty end\''
 
-        # Filter checks if subdomain is on 'name' or 'value' field within FDNS file
-        jq_filter = f'if (.name | test("{jq_item}")) then .name elif (.value | test("{jq_item}")) then .value else empty end'
-
-        p = subprocess.run([
-            'jq', '-crM', jq_filter
-                # Returns 'bytes' type
-                # -c: compact instead of pretty-printed output;
-                # -r: output raw strings, not JSON texts;
-                # -M: monochrome (don't colorize JSON);
-        ],
-        capture_output=True, input=p.stdout)
-
-        p = subprocess.run(['sort'], capture_output=True, input=p.stdout)
-        p = subprocess.run(['uniq'], capture_output=True, input=p.stdout)
+        p = subprocess.run([f'zcat {fdns_file}\
+            | grep -F {grep_item}\
+            | jq -crM {jq_filter}\
+            | sort\
+            | uniq\
+            '], capture_output=True, shell=True, text=True)
 
         fdns_outp_list.extend(p.stdout.split('\n'))
 
