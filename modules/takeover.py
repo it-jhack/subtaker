3ab67c106bbf9b88140f9ef1751e3fac31bd5a1a
Thiago A <it.eng.amaraldepaula@gmail.com>
2021-05-11 13:42:37 -0300

    fix: output inconsistencies

--- modules/takeover.py
+++ modules/takeover.py
@@ -18,7 +18,7 @@ def get_httprobe(subdomains_file):
 
 
 def update_nuclei_templates():
-    nuclei_upd = ['nuclei', '-update-templates']
+    nuclei_upd = ['nuclei', '-update-templates', '-silent']
 
     p = subprocess.run(nuclei_upd)
 
@@ -38,15 +38,16 @@ def get_nuclei(subdomains_file, output_path, resolvers):
 
     nuclei_cmd = [
         'nuclei',
+        # '-silent'
+        # '-verbose'
+        #'-no-color',
         '-t', 'takeovers',
-        '-no-color',
         '-o', output_path, # writes file
         '-r', resolvers,
-        '-rate-limit', '150', # default = 150
-        '-silent',
+        '-rate-limit', '150' # default = 150
     ]
     
-    nuclei_p = subprocess.run(nuclei_cmd, capture_output=True, input=httprobe_urls)
+    nuclei_p = subprocess.run(nuclei_cmd, input=httprobe_urls)
 
     # Already writes file (-o cmd option)
     # returning stdout in case of print() intended
--- subtaker.py
+++ subtaker.py
@@ -94,7 +94,8 @@ print('''
         |___/               |___/                      
 ''')
 
-print("\nLEGAL WARNING: DO NOT USE THIS TOOL ON WEBSITES YOU DON'T HAVE PERMISSION!\n")
+print("\nLEGAL WARNING:\tDO NOT USE THIS TOOL ON ASSETS YOU DON'T HAVE PERMISSION!\n"
+    + "\t\tPROCEED AT YOUR OWN RESPONSIBILITY!")
 
 
 # 0.1 Updating resolvers
@@ -106,16 +107,12 @@ except FileNotFoundError:
 
 try:
     # Fresh list of periodically validated public DNS resolvers.
-    subprocess.run(['wget', 'https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt',
+    subprocess.run(['wget', '--no-verbose', 'https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt',
         '-P', get_resolvers_dir()])
 
-    modules.utils.screen_msg("updated DNS resolvers file successfully")
+    modules.utils.screen_msg("DNS resolvers file updated successfully!\n")
 except Exception:
     modules.utils.screen_msg(f"resolvers file could not be updated at {get_resolvers_dir()}")
-
-
-print('\n> Always remember to check for FDNS file UPDATES on:')
-print(colored('  https://opendata.rapid7.com/sonar.fdns_v2/\n', 'blue'))
 
 
 # Timestamp for file names
@@ -148,15 +145,20 @@ if args.amass_enum:
         subdomains_list.extend(list(unique_everseen(modules.amass_enum.run_amass_enum(domain))))
 
     subdomains_list = list(unique_everseen(subdomains_list))
+    print()
 
 
 #! Retrieve subdomains from FDNS related to inscope domains
 if args.fdns:
-    modules.utils.screen_msg(f'Grepping {args.fdns} for subdomains')
+    modules.utils.screen_msg(f'Grepping for subdomains on file: {args.fdns}')
+    print('\n> Always remember to check for FDNS file UPDATES on:')
+    print(colored('  https://opendata.rapid7.com/sonar.fdns_v2/\n', 'green'))
     
     fdns_output = []
-    fdns_output.extend(modules.fdns.grep_subds_fdns(grep_domains_list, args.fdns))
-    fdns_output = list(unique_everseen(fdns_output))
+    for domain in grep_domains_list:
+        modules.utils.screen_msg(f'Grepping \'{domain}\'')
+        fdns_output.extend(modules.fdns.grep_subd_fdns(domain, args.fdns))
+        fdns_output = list(unique_everseen(fdns_output))
     
     modules.utils.screen_msg(f'FDNS grep ended succesfully! '
         + f'{modules.utils.count_new_unique(subdomains_list, fdns_output):,d}'
@@ -164,13 +166,19 @@ if args.fdns:
         + f'{len(fdns_output):,d}'
         + ' found on .json.gz file.')
     
+    print()#TODO DEL
+    print(f'len(subdomains_list) == {len(subdomains_list):,d}')#TODO DEL
+    print(f'len(fdns_output) == {len(fdns_output):,d}')#TODO DEL
     subdomains_list = modules.utils.merge_lists(subdomains_list, fdns_output)
+    print(f'subdomains_list = modules.utils.merge_lists(subdomains_list, fdns_output)')#TODO DEL
+    print(f'len(subdomains_list) == {len(subdomains_list):,d}')#TODO DEL
+    print(f'len(fdns_output) == {len(fdns_output):,d}')#TODO DEL
     
     #!TODO BEGIN DEL TEST
     from datetime import datetime
     base_dir = f'/tmp/'
-    ftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
-    test_file = f'{base_dir}{ftimestamp}-test_fdns_output-append_list'
+    testftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
+    test_file = f'{base_dir}{testftimestamp}-test_fdns_output-append_list'
     with open(test_file, 'a') as f:
         for item in fdns_output:
             f.write(item + '\n')
@@ -180,33 +188,39 @@ if args.fdns:
 
 #! Bruteforce
 if args.brute:
-    
-    brute_list = []
+    print()
+    modules.utils.screen_msg(f'Generating brute-force subdomains using wordlist at: {args.brute}')
 
-    modules.utils.screen_msg(f'Subdomain brute-forcing using wordlist at: {args.brute}')
-    
-    with open(args.brute, 'r') as brute_file:
-        for line in brute_file:
-            for domain in grep_domains_list:
-                brute_list.append(line + '.' + domain)
+    for domain in grep_domains_list:
+        brute_sublist = []
+        with open(args.brute, 'r') as f:
+            for line in f:
+                brute_sublist.append(line.strip('\n') + '.' + domain)
+        subdomains_list.extend(brute_sublist)
         
-        subdomains_list = modules.utils.merge_lists(subdomains_list, brute_list)
+    print(f'len(subdomains_list == {len(subdomains_list):,d}')#TODO DEL
+    subdomains_list = list(unique_everseen(subdomains_list))
+    print('subdomains_list = list(unique_everseen(subdomains_list))')#TODO DEL
+    print(f'len(subdomains_list == {len(subdomains_list):,d}')#TODO DEL
+    del brute_sublist
+
+        # subdomains_list = modules.utils.merge_lists(subdomains_list, brute_list) #TODO DEL
         
         #!TODO BEGIN DEL TEST
-        from datetime import datetime
-        base_dir = f'/tmp/'
-        ftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
-        test_file = f'{base_dir}{ftimestamp}-test_brute_list-append_list'
-        with open(test_file, 'a') as f:
-            for item in brute_list:
-                f.write(item + '\n')
+        # from datetime import datetime
+        # base_dir = f'/tmp/'
+        # testftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
+        # test_file = f'{base_dir}{testftimestamp}-test_brute_list-append_list'
+        # with open(test_file, 'a') as f:
+        #     for item in brute_list:
+        #         f.write(item + '\n')
         #!TODO END DEL TEST
-        del brute_list
+        # del brute_list
 
 
 #! Massdns: resolve subdomains
 print()
-modules.utils.screen_msg(f'Starting MassDNS on {len(subdomains_list):,d} possible subdomains'
+modules.utils.screen_msg(f'Starting Massdns on {len(subdomains_list):,d} possible subdomains'
     + f' with {args.concurrent:,d} concurrent DNS lookups.')
 
 
@@ -221,8 +235,8 @@ modules.massdns.massdns_dump(subdomains_list, resolvers_f, args.concurrent, mass
 #!TODO BEGIN DEL TEST
 from datetime import datetime
 base_dir = f'/tmp/'
-ftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
-test_file = f'{base_dir}{ftimestamp}-test_subdomains_list-append_list'
+testftimestamp = datetime.now().strftime('%y%m%d-%H%M%S')
+test_file = f'{base_dir}{testftimestamp}-test_subdomains_list-append_list'
 with open(test_file, 'a') as f:
     for item in subdomains_list:
         f.write(item + '\n')
@@ -236,7 +250,7 @@ massdns_txt_subds_l.extend(modules.massdns.grep_subds_ndjson(massdns_ndjson_outp
 modules.utils.append_list_file(massdns_txt_subds_l, massdns_txt_subds_outpf)
 
 
-# Stats
+# Massdns Stats
 line_count = modules.utils.count_lines(massdns_ndjson_outpf)
 
 cname_count = subprocess.run(['grep', '-c', 'CNAME', massdns_ndjson_outpf],
@@ -245,7 +259,7 @@ cname_count = subprocess.run(['grep', '-c', 'CNAME', massdns_ndjson_outpf],
 mx_count = subprocess.run(['grep', '-c', 'MX', massdns_ndjson_outpf],
     capture_output=True)
 
-modules.utils.screen_msg('MassDNS finished!\n'
+modules.utils.screen_msg('Massdns finished!\n'
     + f'\t{line_count:,d} total records retrieved\n'
     + f'\t{int(cname_count.stdout):,d} CNAME records\n'
     + f'\t{int(mx_count.stdout):,d} MX records')
@@ -255,17 +269,30 @@ modules.utils.screen_msg('MassDNS finished!\n'
     # Nuclei checks urls for 'takeovers' yaml templates
 
 # First, update Nuclei templates
+print()
+modules.utils.screen_msg('Updating Nuclei templates')
 modules.takeover.update_nuclei_templates()
 
 # Nuclei output file path
 nuclei_outpf = f'{args.out_dir}{ftimestamp}-possible-takeovers.txt'
 
-
-# Then, run
+# Then, run nuclei
+print()
+modules.utils.screen_msg('Running nuclei: testing for subdomain takeover')
 modules.takeover.get_nuclei(massdns_txt_subds_outpf,
     nuclei_outpf,
     resolvers_f)
 
-modules.utils.screen_msg('Subtaker scan finished!\n'
-    + f'\t Reports base file: {base_filepath}')
+# Getting n of possible takeovers
+takeovers_int = 0
+try:
+    takeovers_int += modules.utils.count_lines(nuclei_outpf)
 
+# Nuclei does not create a file if no result was found
+except FileNotFoundError:
+    takeovers_int += 0
+
+print()
+modules.utils.screen_msg('Subtaker scan finished!\n'
+    + f'\t{takeovers_int:,d} results found'
+    + f'\tReport files base path: {base_filepath}')
